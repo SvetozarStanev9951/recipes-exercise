279241034eff1e6bd2213e1d59adf27cbfa08e83
Svetozar Stanev <svetozar.stanev@gmail.com>
2022-12-03 19:29:55 +0200

    fetching meals by category

--- scripts/fetchData.js
+++ scripts/fetchData.js
@@ -1,5 +1,7 @@
 const apiBaseUrl = "https://www.themealdb.com/api/json/v1/1";
 const categoriesEndpoint = "/categories.php";
+const filterEndpoint = "/filter.php";
+// https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood
 
 async function fetchData(url) {
   try {
@@ -11,5 +13,16 @@ async function fetchData(url) {
   }
 }
 
-export { apiBaseUrl, categoriesEndpoint };
+async function fetchMealsByCategory(category) {
+  try {
+    const url = apiBaseUrl + filterEndpoint + "?c=" + category;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export { apiBaseUrl, categoriesEndpoint, fetchMealsByCategory };
 export default fetchData;
--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,43 @@
-import fetchData, { apiBaseUrl, categoriesEndpoint } from "./fetchData.js";
+import fetchData, {
+  apiBaseUrl,
+  categoriesEndpoint,
+  fetchMealsByCategory,
+} from "./fetchData.js";
 
 const categoriesFilterDiv = document.getElementById(
   "detailed-categories-filter"
 );
+const resultsContainer = document.getElementById("results-contaienr");
+
+function createMealPreviewElement(meal) {
+  const { idMeal, strMealThumb } = meal;
+  const recipeDiv = document.createElement("div");
+  recipeDiv.className = "category-box";
+  recipeDiv.setAttribute("id", idMeal);
+
+  const recipeImg = document.createElement("img");
+  recipeImg.setAttribute("src", strMealThumb);
+
+  recipeDiv.appendChild(recipeImg);
+  resultsContainer.appendChild(recipeDiv);
+}
+
+async function showMealsByCategory(category) {
+  const { meals } = await fetchMealsByCategory(category);
+
+  resultsContainer.innerHTML = "";
+
+  meals.forEach((recipe) => {
+    createMealPreviewElement(recipe);
+  });
+}
 
 function createCategoryElement(categoryObj) {
   const { strCategory: title, strCategoryThumb: imgSrc } = categoryObj;
 
   const categoryDiv = document.createElement("div");
   categoryDiv.className = "category-box";
+  categoryDiv.addEventListener("click", () => showMealsByCategory(title));
 
   const categoryThumb = document.createElement("img");
   categoryThumb.setAttribute("src", imgSrc);
