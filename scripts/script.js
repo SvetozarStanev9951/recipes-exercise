77442b1e255e649d432712b7feb023eff1f8d489
Svetozar Stanev <svetozar.stanev@gmail.com>
2022-12-10 17:32:12 +0200

    wip

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,11 @@ import fetchData, {
   categoriesEndpoint,
   fetchMealsByCategory,
 } from "./fetchData.js";
+import {
+  readFromStorage,
+  writeToStorage,
+  storageKeys,
+} from "./storageControl.js";
 
 const categoriesFilterDiv = document.getElementById(
   "detailed-categories-filter"
@@ -10,7 +15,7 @@ const categoriesFilterDiv = document.getElementById(
 const resultsContainer = document.getElementById("results-contaienr");
 
 function createMealPreviewElement(meal) {
-  const { idMeal, strMealThumb } = meal;
+  const { idMeal, strMealThumb, strMeal } = meal;
   const recipeDiv = document.createElement("div");
   recipeDiv.className = "category-box";
   recipeDiv.setAttribute("id", idMeal);
@@ -18,7 +23,11 @@ function createMealPreviewElement(meal) {
   const recipeImg = document.createElement("img");
   recipeImg.setAttribute("src", strMealThumb);
 
+  const recipeTitle = document.createElement("h4");
+  recipeTitle.textContent = strMeal;
+
   recipeDiv.appendChild(recipeImg);
+  recipeDiv.appendChild(recipeTitle);
   resultsContainer.appendChild(recipeDiv);
 }
 
@@ -52,7 +61,16 @@ function createCategoryElement(categoryObj) {
 }
 
 async function main() {
-  const { categories } = await fetchData(apiBaseUrl + categoriesEndpoint);
+  let categories = [];
+  categories = readFromStorage(storageKeys.categories);
+
+  if (!categories) {
+    const { categories: remoteCategories } = await fetchData(
+      apiBaseUrl + categoriesEndpoint
+    );
+    categories = remoteCategories;
+    writeToStorage(storageKeys.categories, categories);
+  }
 
   categories.forEach((el) => {
     const newCategoryEl = createCategoryElement(el);
@@ -61,3 +79,11 @@ async function main() {
 }
 
 main();
+
+// 1. Add title for each recipe
+// 2. Build recipes view and fetch data for each recipe
+// 3. Handle search
+// 4. Reset initial view when logo is clicked
+// 5. Recipe of the day
+// 6. Filter by country
+// [DONE] 7. Cache data (local storage)
